62e7ed813d36381194ebf368e15cee29261fd7c5
Daria Soloveva <Daria_Soloveva@epam.com>
2019-02-05 11:10:46 +0300

    http -> https

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ fetch('http://amishin.pythonanywhere.com/', {
   mode: "cors"
 })
   .then(function(response) {
-    console.log(`http://amishin.pythonanywhere.com/ ${response}`);
+    console.log(`https://amishin.pythonanywhere.com/ ${response}`);
   });
 
 fetch('https://www.edmunds.com/api/inventory/v5/find?zip=44241&sortby=_geo&radius=500&pagenum=1&pagesize=3&type=USED&vehicleInfo.styleInfo.make=Mazda&vehicleInfo.styleInfo.model=CX-3')
